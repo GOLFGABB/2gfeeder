1fee01ee689e8b19ef41078883a6f1448b6976c3
GOLFGABB <golfgab@outlook.com>
2018-08-09 20:33:53 +0700

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgForm} from '@angular/forms';
+import {AngularFireDatabase} from 'angularfire2/database';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private db: AngularFireDatabase){}
+  addForm(data: NgForm){
+    this.db.object("/machines").set(data.value);
+    alert('บันทึกเรียบร้อยแล้วจ้า');
+  }
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA89IHwEjN4wpEN7UoLq-MBhT03poDK2mk",
+    authDomain: "gfeeder-c8cf8.firebaseapp.com",
+    databaseURL: "https://gfeeder-c8cf8.firebaseio.com",
+    projectId: "gfeeder-c8cf8",
+    storageBucket: "gfeeder-c8cf8.appspot.com",
+    messagingSenderId: "162992263017"
+}
